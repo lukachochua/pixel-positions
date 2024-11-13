9463363f9d4f6624bcbd53e155a48061e18144bc
lukachochua <91949321+lukachochua@users.noreply.github.com>
2024-11-13 17:12:05 +0400

    day 27 - from design to blade

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ export default {
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
-        './resources/**/*.vue',
     ],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "black": "#060606"
             },
         },
     },
